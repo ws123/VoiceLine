4e4bdb947cc68d65ace447fededd39fde9ebf331
carlos1992 <372532647@qq.com>
2016-01-29 15:56:05 +0800

    first init

---
+++ lib/src/main/java/com/carlos/voiceline/lib/VoiceLineView.java
@@ -0,0 +1,22 @@
+package com.carlos.voiceline.lib;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2016/1/29.
+ */
+public class VoiceLineView extends View {
+    public VoiceLineView(Context context) {
+        super(context);
+    }
+
+    public VoiceLineView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public VoiceLineView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
